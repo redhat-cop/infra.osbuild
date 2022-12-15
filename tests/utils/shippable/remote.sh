15453306e840e1033411f03013bbbceef3e033e9
matoval <mat24c@gmail.com>
2022-12-15 11:17:10 -0800

    Revert retry on error, fix container to fedora, fix installer check

--- tests/utils/shippable/remote.sh
+++ tests/utils/shippable/remote.sh
@@ -25,5 +25,5 @@ if [ "${platform}" == "rhel" ] && [[ "${version}" =~ ^8 ]]; then
 fi
 
 # shellcheck disable=SC2086
-ansible-test integration --color -v "${target}" ${COVERAGE:+"$COVERAGE"} ${CHANGED:+"$CHANGED"} ${UNSTABLE:+"$UNSTABLE"} \
+ansible-test integration --color -v --retry-on-error "${target}" ${COVERAGE:+"$COVERAGE"} ${CHANGED:+"$CHANGED"} ${UNSTABLE:+"$UNSTABLE"} \
     --remote "${platform}/${version}" --remote-terminate always --remote-stage "${stage}" --remote-provider "${provider}"
