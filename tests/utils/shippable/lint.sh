ededc33320f0fd1e413f6b7941971ce92e18d370
Adam Miller <admiller@redhat.com>
2023-01-12 11:45:38 -0600

    don't lock on old version of anislbe-lint, issue was fixed and release
    Signed-off-by: Adam Miller <admiller@redhat.com>

--- tests/utils/shippable/lint.sh
+++ tests/utils/shippable/lint.sh
@@ -6,6 +6,6 @@ echo "${PATH}"
 echo "${HOME}"
 command -v ansible
 
-pip install ansible-lint==6.8.4 --disable-pip-version-check
+pip install ansible-lint --disable-pip-version-check
 
 PATH="${PATH}:${HOME}/.local/bin" ansible-lint --profile=production
