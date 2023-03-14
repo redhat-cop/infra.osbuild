97d68eb7819f6fd1b58ad335ac1b62a6f2179b53
Demetrius Lima <delima@redhat.com>
2023-03-14 18:08:26 -0300

    test

--- .azure-pipelines/scripts/process-results.sh
+++ .azure-pipelines/scripts/process-results.sh
@@ -11,11 +11,28 @@ fi
 
 echo "##vso[task.setVariable variable=outputPath]${output_path}"
 
+#touch "${output_path}"'/bot/file.tar'
+mkdir -p "${output_path}"'/bot/'
+#sudo mv "/root/ansible_collections/infra/osbuild/tests/images"'/*.tar' "${output_path}"'/bot/'
+
+echo "Locations..."
+pwd
+echo "Searching..."
+find ./ -name "*.tar"
+
+if compgen -G "${output_path}"'/bot/*.tar' > /dev/null; then
+    echo "There is images"
+    ls "${output_path}"'/bot/'
+else
+    echo "There is no images"
+fi
+
 if compgen -G "${output_path}"'/junit/*.xml' > /dev/null; then
     echo "##vso[task.setVariable variable=haveTestResults]true"
 fi
 
-if compgen -G "${output_path}"'/bot/ansible-test-*' > /dev/null; then
+if compgen -G "${output_path}"'/bot/*' > /dev/null; then
+    echo "There are bot results"
     echo "##vso[task.setVariable variable=haveBotResults]true"
 fi
 
