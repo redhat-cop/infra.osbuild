02539e432ce3e7cbd35135922a11c93a912cdac7
matoval <mat24c@gmail.com>
2023-04-14 19:32:27 -0700

    Remove unused import

--- tests/unit/plugins/modules/utils.py
+++ tests/unit/plugins/modules/utils.py
@@ -7,7 +7,6 @@ from __future__ import (absolute_import, division, print_function)
 __metaclass = type
 
 from unittest.mock import Mock, MagicMock
-import pytest
 import toml
 
 blueprint_sanity_check_mock = {"errors": []}
