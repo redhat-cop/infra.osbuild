636b92a7cc1b15bf3c66f3ed90c0a4ef4eef67a6
Thom Carlin <tcarlin@redhat.com>
2023-05-09 09:02:18 -0400

    Aap 10697 (#155)
    * AAP-10697: Change from weldr.request.open() to weldr.request.fetch_url() to properly handle API request
    
    * Remove unneeded lint overrides
    
    * Working version for first part
    
    * Corrected post_compose logic to properly return JSON document
    
    * Fix ansible-test sanity
    
    * Fix KeyError during tests
    
    * Typo
    
    * Fix typo in test_start_compose_unable_to_determine_build and various lint issues

--- plugins/module_utils/weldrapiv1.py
+++ plugins/module_utils/weldrapiv1.py
@@ -1,14 +1,13 @@
 #
 # (c) 2022, Adam Miller (admiller@redhat.com)
-# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt) # noqa: E501
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 import json
 import os
 import shutil
 
-import ansible.module_utils.six.moves.urllib.error as urllib_error
 from ansible.module_utils._text import to_bytes
-from ansible.module_utils._text import to_native
 from ansible.module_utils.six.moves.urllib.parse import quote
+from ansible.module_utils.urls import fetch_url
 
 
 class WeldrV1:
@@ -23,7 +22,7 @@ class WeldrV1:
         initializer
 
         :module:          AnsibleModule, instance of AnsibleModule
-        :unix_socket:     string, local UNIX Socket that Weldr service is listening on # noqa: E501
+        :unix_socket:     string, local UNIX Socket that Weldr service is listening on
         """
         self.weldr = weldr
 
@@ -45,11 +44,7 @@ class WeldrV1:
 
         :return:        dict
         """
-        results = json.load(
-            self.weldr.request.open(
-                "GET", "http://localhost/api/v1/projects/source/list"
-            )  # noqa: E501
-        )
+        results = json.load(self.weldr.request.open("GET", "http://localhost/api/v1/projects/source/list"))
         return results
 
     def get_projects_source_info(self, repo_name):
@@ -62,7 +57,7 @@ class WeldrV1:
         results = json.load(
             self.weldr.request.open(
                 "GET",
-                "http://localhost/api/v1/projects/source/info/%s" % quote(repo_name),  # noqa: E501
+                "http://localhost/api/v1/projects/source/info/%s" % quote(repo_name),
             )
         )
         return results
@@ -77,7 +72,7 @@ class WeldrV1:
         results = json.load(
             self.weldr.request.open(
                 "GET",
-                "http://localhost/api/v1/projects/source/info/%s" % quote(repo_name),  # noqa: E501
+                "http://localhost/api/v1/projects/source/info/%s" % quote(repo_name),
                 headers={"Content-Type": "application/json"},
             )
         )
@@ -112,8 +107,7 @@ class WeldrV1:
         results = json.load(
             self.weldr.request.open(
                 "DELETE",
-                "http://localhost/api/v1/projects/source/delete/%s"
-                % quote(repo_name),  # noqa: E501
+                "http://localhost/api/v1/projects/source/delete/%s" % quote(repo_name),
                 headers={"Content-Type": "application/json"},
             )
         )
@@ -122,13 +116,13 @@ class WeldrV1:
     # Projects depsolve
     def get_projects_depsolve(self):
         """
-        # api.router.GET("/api/v:version/projects/depsolve", api.projectsDepsolveHandler)  # noqa: E501
+        # api.router.GET("/api/v:version/projects/depsolve", api.projectsDepsolveHandler)
         """
         raise NotImplementedError
 
     def get_projects_depsolve_projects(self, projects):
         """
-        # api.router.GET("/api/v:version/projects/depsolve/*projects", api.projectsDepsolveHandler)  # noqa: E501
+        # api.router.GET("/api/v:version/projects/depsolve/*projects", api.projectsDepsolveHandler)
         """
         raise NotImplementedError
 
@@ -139,31 +133,29 @@ class WeldrV1:
 
         :return:        list, list of modules dicts
         """
-        results = json.load(
-            self.weldr.request.open("GET", "http://localhost/api/v1/modules/list")  # noqa: E501
-        )  # noqa: E501
+        results = json.load(self.weldr.request.open("GET", "http://localhost/api/v1/modules/list"))
         return results
 
     def get_modules_list_modules(self, modules):
         """
-        # api.router.GET("/api/v:version/modules/list/*modules", api.modulesListHandler)  # noqa: E501
+        # api.router.GET("/api/v:version/modules/list/*modules", api.modulesListHandler)
         """
         raise NotImplementedError
 
     def get_projects_list(self):
         """
-        # api.router.GET("/api/v:version/projects/list", api.projectsListHandler)  # noqa: E501
+        # api.router.GET("/api/v:version/projects/list", api.projectsListHandler)
         """
         raise NotImplementedError
 
     def get_projects_lists(self):
         """
-        # api.router.GET("/api/v:version/projects/list/", api.projectsListHandler)  # noqa: E501
+        # api.router.GET("/api/v:version/projects/list/", api.projectsListHandler)
         """
         raise NotImplementedError
 
     # Modules/projects info with dependencies
-    # // these are the same, except that modules/info also includes dependencies  # noqa: E501
+    # // these are the same, except that modules/info also includes dependencies
     def get_modules_info(self):
         """
         # api.router.GET("/api/v:version/modules/info", api.modulesInfoHandler)
@@ -172,19 +164,19 @@ class WeldrV1:
 
     def get_modules_info_modules(self, modules):
         """
-        # api.router.GET("/api/v:version/modules/info/*modules", api.modulesInfoHandler)  # noqa: E501
+        # api.router.GET("/api/v:version/modules/info/*modules", api.modulesInfoHandler)
         """
         raise NotImplementedError
 
     def get_projects_info(self):
         """
-        # api.router.GET("/api/v:version/projects/info", api.modulesInfoHandler)  # noqa: E501
+        # api.router.GET("/api/v:version/projects/info", api.modulesInfoHandler)
         """
         raise NotImplementedError
 
     def get_projects_info_modules(self, modules):
         """
-        # api.router.GET("/api/v:version/projects/info/*modules", api.modulesInfoHandler)  # noqa: E501
+        # api.router.GET("/api/v:version/projects/info/*modules", api.modulesInfoHandler)
         """
         raise NotImplementedError
 
@@ -195,9 +187,7 @@ class WeldrV1:
 
         :return:    dict
         """
-        results = json.load(
-            self.weldr.request.open("GET", "http://localhost/api/v1/blueprints/list")  # noqa: E501
-        )
+        results = json.load(self.weldr.request.open("GET", "http://localhost/api/v1/blueprints/list"))
         return results
 
     def get_blueprints_info(self, blueprint):
@@ -206,40 +196,36 @@ class WeldrV1:
 
         :return:    dict
         """
-        results = json.load(
-            self.weldr.request.open(
-                "GET", "http://localhost/api/v1/blueprints/info/%s" % blueprint
-            )  # noqa: E501
-        )
+        results = json.load(self.weldr.request.open("GET", "http://localhost/api/v1/blueprints/info/%s" % blueprint))
         return results
 
     def get_blueprints_depsolve(self, blueprints):
         """
-        # api.router.GET("/api/v:version/blueprints/depsolve/*blueprints", api.blueprintsDepsolveHandler)  # noqa: E501
+        # api.router.GET("/api/v:version/blueprints/depsolve/*blueprints", api.blueprintsDepsolveHandler)
         """
         raise NotImplementedError
 
     def get_blueprints_freeze(self, blueprints):
         """
-        # api.router.GET("/api/v:version/blueprints/freeze/*blueprints", api.blueprintsFreezeHandler)  # noqa: E501
+        # api.router.GET("/api/v:version/blueprints/freeze/*blueprints", api.blueprintsFreezeHandler)
         """
         raise NotImplementedError
 
     def get_blueprints_diff(self, blueprint_from, blueprint_to):
         """
-        # api.router.GET("/api/v:version/blueprints/diff/:blueprint/:from/:to", api.blueprintsDiffHandler)  # noqa: E501
+        # api.router.GET("/api/v:version/blueprints/diff/:blueprint/:from/:to", api.blueprintsDiffHandler)
         """
         raise NotImplementedError
 
     def get_blueprints_change_commit(self, blueprints, commit):
         """
-        # api.router.GET("/api/v:version/blueprints/change/:blueprint/:commit", api.blueprintsChangeHandler)  # noqa: E501
+        # api.router.GET("/api/v:version/blueprints/change/:blueprint/:commit", api.blueprintsChangeHandler)
         """
         raise NotImplementedError
 
     def get_blueprints_changes(self, blueprints):
         """
-        # api.router.GET("/api/v:version/blueprints/changes/*blueprints", api.blueprintsChangesHandler)  # noqa: E501
+        # api.router.GET("/api/v:version/blueprints/changes/*blueprints", api.blueprintsChangesHandler)
         """
         raise NotImplementedError
 
@@ -263,62 +249,63 @@ class WeldrV1:
 
     def post_blueprints_workspace(self, workspace):
         """
-        # api.router.POST("/api/v:version/blueprints/workspace", api.blueprintsWorkspaceHandler)  # noqa: E501
+        # api.router.POST("/api/v:version/blueprints/workspace", api.blueprintsWorkspaceHandler)
         """
         raise NotImplementedError
 
     def post_blueprints_undo_commit(self, blueprint, commit):
         """
-        # api.router.POST("/api/v:version/blueprints/undo/:blueprint/:commit", api.blueprintUndoHandler)  # noqa: E501
+        # api.router.POST("/api/v:version/blueprints/undo/:blueprint/:commit", api.blueprintUndoHandler)
         """
         raise NotImplementedError
 
     def post_blueprints_tag(self, blueprint):
         """
-        # api.router.POST("/api/v:version/blueprints/tag/:blueprint", api.blueprintsTagHandler)  # noqa: E501
+        # api.router.POST("/api/v:version/blueprints/tag/:blueprint", api.blueprintsTagHandler)
         """
         raise NotImplementedError
 
     def delete_blueprints(self, blueprint):
         """
-        # api.router.DELETE("/api/v:version/blueprints/delete/:blueprint", api.blueprintDeleteHandler)  # noqa: E501
+        # api.router.DELETE("/api/v:version/blueprints/delete/:blueprint", api.blueprintDeleteHandler)
         """
         raise NotImplementedError
 
     def delete_blueprints_workspace(self, blueprint):
         """
-        # api.router.DELETE("/api/v:version/blueprints/workspace/:blueprint", api.blueprintDeleteWorkspaceHandler)  # noqa: E501
+        # api.router.DELETE("/api/v:version/blueprints/workspace/:blueprint", api.blueprintDeleteWorkspaceHandler)
         """
         raise NotImplementedError
 
     # Composes
     def post_compose(self, compose_settings):
         """
-        iniiate a compose
+        initiate a compose
 
         :return:    dict
         """
-        results = "FOOBAR"
-        try:
-            if type(compose_settings) != bytes:
-                compose_settings = to_bytes(compose_settings)
-            results = json.load(
-                self.weldr.request.open(
-                    "POST",
-                    "http://localhost/api/v1/compose",
-                    data=compose_settings,
-                    headers={"Content-Type": "application/json"},
-                )
-            )
-            return results
-        except urllib_error.HTTPError as e:
-            self.weldr.module.fail_json(
-                msg="OSBUILD COMPOSER ERROR: %s" % to_native(e.reason)
-            )  # noqa: E501
+        if type(compose_settings) != bytes:
+            compose_settings = to_bytes(compose_settings)
+        response, info = fetch_url(
+            module=self.weldr.module,
+            url="http://localhost/api/v1/compose",
+            method="POST",
+            data=compose_settings,
+            headers={"Content-Type": "application/json"},
+            unix_socket=self.weldr.unix_socket,
+        )
+        result = dict()
+        result["status_code"] = info["status"]
+        if info["status_code"] < 400:
+            result["body"] = json.loads(response.read().decode("utf-8"))
+        else:
+            result["body"] = info["body"]
+            result["error_msg"] = info["msg"]
+        return result
 
     def delete_compose(self, compose):
         """
-        # api.router.DELETE("/api/v:version/compose/delete/:uuids", api.composeDeleteHandler)  # noqa: E501
+        # api.router.DELETE("/api/v:version/compose/delete/:uuids", api.composeDeleteHandler)
         """
         raise NotImplementedError
 
@@ -328,9 +315,7 @@ class WeldrV1:
 
         :return:        dict
         """
-        results = json.load(
-            self.weldr.request.open("GET", "http://localhost/api/v1/compose/types")  # noqa: E501
-        )
+        results = json.load(self.weldr.request.open("GET", "http://localhost/api/v1/compose/types"))
         return results
 
     def get_compose_queue(self):
@@ -339,9 +324,7 @@ class WeldrV1:
 
         :return:        dict
         """
-        results = json.load(
-            self.weldr.request.open("GET", "http://localhost/api/v1/compose/queue")  # noqa: E501
-        )
+        results = json.load(self.weldr.request.open("GET", "http://localhost/api/v1/compose/queue"))
         return results
 
     def get_compose_status(self, compose_uuids):
@@ -351,11 +334,7 @@ class WeldrV1:
 
         :return:        dict
         """
-        results = json.load(
-            self.weldr.request.open(
-                "GET", "http://localhost/api/v1/compose/status/%s" % compose_uuids  # noqa: E501
-            )
-        )
+        results = json.load(self.weldr.request.open("GET", "http://localhost/api/v1/compose/status/%s" % compose_uuids))
         return results
 
     def get_compose_info(self, compose_uuid):
@@ -365,40 +344,32 @@ class WeldrV1:
 
         :return:        dict
         """
-        results = json.load(
-            self.weldr.request.open(
-                "GET", "http://localhost/api/v1/compose/info/%s" % compose_uuid
-            )  # noqa: E501
-        )
+        results = json.load(self.weldr.request.open("GET", "http://localhost/api/v1/compose/info/%s" % compose_uuid))
         return results
 
     def get_compose_finished(self):
         """
-        # api.router.GET("/api/v:version/compose/finished", api.composeFinishedHandler)  # noqa: E501
+        # api.router.GET("/api/v:version/compose/finished", api.composeFinishedHandler)
         query info of completed composes
 
         :return:        dict
         """
-        results = json.load(
-            self.weldr.request.open("GET", "http://localhost/api/v1/compose/finished")  # noqa: E501
-        )
+        results = json.load(self.weldr.request.open("GET", "http://localhost/api/v1/compose/finished"))
         return results
 
     def get_compose_failed(self):
         """
-        # api.router.GET("/api/v:version/compose/failed", api.composeFailedHandler)  # noqa: E501
+        # api.router.GET("/api/v:version/compose/failed", api.composeFailedHandler)
         query list of failed composes
 
         :return:        dict
         """
-        results = json.load(
-            self.weldr.request.open("GET", "http://localhost/api/v1/compose/failed")  # noqa: E501
-        )
+        results = json.load(self.weldr.request.open("GET", "http://localhost/api/v1/compose/failed"))
         return results
 
     def get_compose_image(self, compose_uuid, dest):
         """
-        # api.router.GET("/api/v:version/compose/image/:uuid", api.composeImageHandler)  # noqa: E501
+        # api.router.GET("/api/v:version/compose/image/:uuid", api.composeImageHandler)
         """
         tmpfile = self.weldr.fetch_file(
             self.weldr.module,
@@ -414,87 +385,87 @@ class WeldrV1:
 
     def get_compose_metadata(self, compose_uuid):
         """
-        # api.router.GET("/api/v:version/compose/metadata/:uuid", api.composeMetadataHandler)  # noqa: E501
+        # api.router.GET("/api/v:version/compose/metadata/:uuid", api.composeMetadataHandler)
         """
         raise NotImplementedError
 
     def get_compose_results(self, compose_uuid):
         """
-        # api.router.GET("/api/v:version/compose/results/:uuid", api.composeResultsHandler)  # noqa: E501
+        # api.router.GET("/api/v:version/compose/results/:uuid", api.composeResultsHandler)
         """
         raise NotImplementedError
 
     def get_compose_logs(self, compose_uuid):
         """
-        # api.router.GET("/api/v:version/compose/logs/:uuid", api.composeLogsHandler)  # noqa: E501
+        # api.router.GET("/api/v:version/compose/logs/:uuid", api.composeLogsHandler)
         """
         raise NotImplementedError
 
     def get_compose_log(self, compose_uuid):
         """
-        # api.router.GET("/api/v:version/compose/log/:uuid", api.composeLogHandler)  # noqa: E501
+        # api.router.GET("/api/v:version/compose/log/:uuid", api.composeLogHandler)
         """
         raise NotImplementedError
 
     def post_compose_uploads_schedule(self, compose_uuid):
         """
-        # api.router.POST("/api/v:version/compose/uploads/schedule/:uuid", api.uploadsScheduleHandler)  # noqa: E501
+        # api.router.POST("/api/v:version/compose/uploads/schedule/:uuid", api.uploadsScheduleHandler)
         """
         raise NotImplementedError
 
     def compose_cancel(self, compose_uuid):
         """
-        # api.router.DELETE("/api/v:version/compose/cancel/:uuid", api.composeCancelHandler)  # noqa: E501
+        # api.router.DELETE("/api/v:version/compose/cancel/:uuid", api.composeCancelHandler)
         """
         raise NotImplementedError
 
     # Uploads
     def delete_compose_upload(self, compose_uuid):
         """
-        # api.router.DELETE("/api/v:version/upload/delete/:uuid", api.uploadsDeleteHandler)  # noqa: E501
+        # api.router.DELETE("/api/v:version/upload/delete/:uuid", api.uploadsDeleteHandler)
         """
         raise NotImplementedError
 
     def get_version_info(self, compose_uuid):
         """
-        # api.router.GET("/api/v:version/upload/info/:uuid", api.uploadsInfoHandler)  # noqa: E501
+        # api.router.GET("/api/v:version/upload/info/:uuid", api.uploadsInfoHandler)
         """
         raise NotImplementedError
 
     def get_version_upload_log(self, compose_uuid):
         """
-        # api.router.GET("/api/v:version/upload/log/:uuid", api.uploadsLogHandler)  # noqa: E501
+        # api.router.GET("/api/v:version/upload/log/:uuid", api.uploadsLogHandler)
         """
         raise NotImplementedError
 
     def upload_reset(self, compose_uuid):
         """
-        # api.router.POST("/api/v:version/upload/reset/:uuid", api.uploadsResetHandler)  # noqa: E501
+        # api.router.POST("/api/v:version/upload/reset/:uuid", api.uploadsResetHandler)
         """
         raise NotImplementedError
 
     def upload_cancel(self, compose_uuid):
         """
-        # api.router.DELETE("/api/v:version/upload/cancel/:uuid", api.uploadsCancelHandler)  # noqa: E501
+        # api.router.DELETE("/api/v:version/upload/cancel/:uuid", api.uploadsCancelHandler)
         """
         raise NotImplementedError
 
     # Upload providers
     def get_upload_providers(self):
         """
-        # api.router.GET("/api/v:version/upload/providers", api.providersHandler)  # noqa: E501
+        # api.router.GET("/api/v:version/upload/providers", api.providersHandler)
         """
         raise NotImplementedError
 
     def save_providers(self):
         """
-        # api.router.POST("/api/v:version/upload/providers/save", api.providersSaveHandler)  # noqa: E501
+        # api.router.POST("/api/v:version/upload/providers/save", api.providersSaveHandler)
         """
         raise NotImplementedError
 
     def delete_provider(self, provider, profile):
         """
-        # api.router.DELETE("/api/v:version/upload/providers/delete/:provider/:profile", api.providersDeleteHandler)  # noqa: E501
+        # api.router.DELETE("/api/v:version/upload/providers/delete/:provider/:profile", api.providersDeleteHandler)
         """
         raise NotImplementedError
 
