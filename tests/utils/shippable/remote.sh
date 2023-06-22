274dd8c77b9d96759d7fd5f1e92a194b87de343b
Thom Carlin <tcarlin@redhat.com>
2023-06-22 09:29:21 -0400

    RHEL 8.7 try Python 3.8

--- tests/utils/shippable/remote.sh
+++ tests/utils/shippable/remote.sh
@@ -31,7 +31,7 @@ elif [ "${platform}" == "fedora" ] && [ "${version}" == "38" ]; then
 elif [ "${platform}" == "rhel" ] && [ "${version}" == "8.7" ]; then
     PYTHON="--python 3.6"
 elif [ "${platform}" == "rhel" ] && [ "${version}" == "8.8" ]; then
-    PYTHON="--python 3.6"
+    PYTHON="--python 3.8"
 elif [ "${platform}" == "rhel" ] && [ "${version}" == "9.1" ]; then
     PYTHON="--python 3.9"
 elif [ "${platform}" == "rhel" ] && [ "${version}" == "9.2" ]; then
