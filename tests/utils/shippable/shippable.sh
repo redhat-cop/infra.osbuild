1a58702a2299b510fca7c6e5a270c1cecaafc8c2
Thom Carlin <tcarlin@redhat.com>
2023-07-18 18:40:34 -0400

    RHC typo fix attempt 2

--- tests/utils/shippable/shippable.sh
+++ tests/utils/shippable/shippable.sh
@@ -91,7 +91,7 @@ if [ "${script}" != "sanity" ] && [ "${script}" != "units" ] && [ "${test}" != "
     retry git clone --depth=1 --single-branch https://github.com/ansible-collections/ansible.posix.git "${ANSIBLE_COLLECTIONS_PATH}/ansible_collections/ansible/posix"
 
     # retry ansible-galaxy -vvv collection install fedora.linux_system_roles
-    retry git clone --depth=1 --single-branch --single-branch https://github.com/linux-system-roles/rhc.git "${ANSIBLE_COLLECTIONS_PATH}/ansible_collections/linux-system-roles/rhc"
+    retry git clone --depth=1 --single-branch --single-branch https://github.com/linux-system-roles/rhc.git "${ANSIBLE_COLLECTIONS_PATH}/ansible_collections/rhc"
 
 fi
 
