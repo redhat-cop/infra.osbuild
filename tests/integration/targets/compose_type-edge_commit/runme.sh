ed2cc8c7b102c9cedb7b8a0e4f7fd29d653bd546
Thom Carlin <tcarlin@redhat.com>
2023-07-21 15:00:51 -0400

    RHC typo fix attempt 43

---
+++ tests/integration/targets/compose_type-edge_commit/runme.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+set -eux
+
+ANSIBLE_ROLES_PATH="${ANSIBLE_ROLES_PATH}:/usr/share/ansible/roles" ansible-playbook tests/integration/targets/compose-type/edge_commit/tasks/main.yml -i ../../inventory "$@"
