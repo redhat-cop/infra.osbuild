a955ba33c0b7f54591781d7633d63d818ea29e94
Lester Claudio <claudiol@redhat.com>
2024-08-29 10:10:45 -0600

    Update python to default

--- tests/utils/shippable/remote.sh
+++ tests/utils/shippable/remote.sh
@@ -32,7 +32,7 @@ ansible-test integration --color -v --retry-on-error \
     ${COVERAGE:+"$COVERAGE"} \
     ${CHANGED:+"$CHANGED"} \
     ${UNSTABLE:+"$UNSTABLE"} \
-    --python 3.11 \
+    --python default \
     --python-interpreter /usr/bin/python3 \
     --remote "${platform}/${version}" \
     --remote-terminate always \
